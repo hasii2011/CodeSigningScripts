6909105df8feeb5688f744a5aa704e2f5fdb9204
Humberto Sanchez II <n3875yffbt@privaterelay.appleid.com>
2022-06-16 14:08:38 -0500

    <Enhancement>[Both main scripts]: <Update paths>
    [Do it the hard way duplicate scripts]
    
    [https://github.com/hasii2011/CodeSigningScripts/issues/3]

---
+++ python310zipsign.sh
@@ -0,0 +1,63 @@
+#!/usr/bin/env bash
+#
+#  Signing the embedded .zip file is necessary because Apple code signing verification is failing
+#  on them.  (Sheesh)
+#
+#
+#  assumes Xcode 13 is installed
+#
+tput clear
+source ./common.sh
+setProjectsBase
+
+PROJECT_DIR=${1}
+APPLICATION_NAME=${2}
+# If either of the parameters do not exist or are not valid the function will
+# print an error messaged and exit the script with a non-zero 
+# status code
+validateParameters ${PROJECT_DIR} ${APPLICATION_NAME}
+
+export FULL_PATH="${PROJECTS_BASE}/${PROJECT_DIR}"
+
+export APP="${FULL_PATH}/dist/${APPLICATION_NAME}.app"
+
+export TEMP_DIR="/tmp"
+export ORIGINAL_ZIP_DIR="${APP}/Contents/Resources/lib"
+export UNZIP_DIR="python310"
+export ZIP_NAME="python310.zip"
+export PYTHON_ZIP="${ORIGINAL_ZIP_DIR}/${ZIP_NAME}"
+export PYTHON_UNZIP_DIR="${TEMP_DIR}/${UNZIP_DIR}"
+
+
+#
+echo "Cleanup temporary directory"
+rm -rf "${TEMP_DIR}/${ZIP_NAME}"
+rm -rf ${PYTHON_UNZIP_DIR}
+#
+echo "Get copy of unsigned zip file"
+cp -vp ${PYTHON_ZIP} ${TEMP_DIR}
+
+echo "Unzip it"
+    /usr/bin/ditto -v -x -k "${TEMP_DIR}/${ZIP_NAME}" "${TEMP_DIR}/${UNZIP_DIR}"
+
+
+export IDENTITY="Developer ID Application: Humberto Sanchez II (NA8Z96F8V9)"
+# export OPTIONS="--verbose --timestamp --options=runtime "
+export OPTIONS="--force --verbose --timestamp --options=runtime "
+
+find "${PYTHON_UNZIP_DIR}/PIL/.dylibs" -iname '*.dylib' |
+    while read libfile; do
+        codesign --sign "${IDENTITY}" "${libfile}" ${OPTIONS}   >> PIL-LibSigning.log 2>&1 ;
+        # codesign --sign "${IDENTITY}" "${libfile}" ${OPTIONS}
+    done;
+
+
+echo "Remove old temp copy zip file"
+rm -vrf "${TEMP_DIR}/${ZIP_NAME}"
+echo "recreate zip file"
+
+/usr/bin/ditto -v -c -k "${TEMP_DIR}/${UNZIP_DIR}" "${TEMP_DIR}/${ZIP_NAME}"
+
+echo "Move signed zip back"
+
+cp -vp "${TEMP_DIR}/${ZIP_NAME}" ${ORIGINAL_ZIP_DIR}
---
+++ signapp310.sh
@@ -0,0 +1,61 @@
+#!/usr/bin/env bash
+
+tput clear
+source ./common.sh
+setProjectsBase
+
+PROJECT_DIR=${1}
+APPLICATION_NAME=${2}
+
+# If either of the parameters do not exist the function will
+# print an error messaged and exit the script with a non-zero 
+# status code
+validateParameters ${PROJECT_DIR} ${APPLICATION_NAME}
+
+
+export APP="${APPLICATION_NAME}.app"
+
+FULL_PROJECT_DIR="${PROJECTS_BASE}/${PROJECT_DIR}"
+FULL_APP_NAME="${FULL_PROJECT_DIR}/dist/${APP}"
+
+export LOGFILE="CodeSigning.log"
+#
+# PyGitIssueClone.app/Contents/Frameworks/liblzma.5.dylib: main executable failed strict validation
+echo "Manually copy from: /usr/local/Cellar/xz/5.2.5/lib/liblzma.5.dylib"
+# https://stackoverflow.com/questions/62095338/py2app-fails-macos-signing-on-liblzma-5-dylib
+#
+export GOOD_LIB='/usr/local/Cellar/xz/5.2.5/lib/liblzma.5.dylib'
+export DIR_TO_OVER_WRITE="${FULL_APP_NAME}/Contents/Frameworks"
+
+# echo "GOOD_LIB: ${GOOD_LIB}"
+# echo "DIR_TO_OVER_WRITE: ${DIR_TO_OVER_WRITE}"
+# This line left for compatability with python & brew on Intel Macs
+cp -vp ${GOOD_LIB} ${DIR_TO_OVER_WRITE}
+#
+#  Ugh code signing will be the death of me
+#
+rm -vrf ${FULL_APP_NAME}/Contents/Resources/lib/python3.9/todoist/.DS_Store
+rm -vrf ${FULL_APP_NAME}/Contents/Resources/lib/python3.9/numpy/f2py/tests/src/assumed_shape/.f2py_f2cmap
+
+#
+# This is a real certificate - expires 21 September 2026
+#
+export IDENTITY="Developer ID Application: Humberto Sanchez II (NA8Z96F8V9)"
+export OPTIONS="--verbose --force --timestamp --options=runtime "
+#
+#  assumes Xcode 13 is installed
+#
+echo "Sign frameworks"
+echo "" > ${LOGFILE}
+codesign --sign "${IDENTITY}" ${OPTIONS} "${FULL_APP_NAME}/Contents/Frameworks/Python.framework/Versions/3.10/Python" >> ${LOGFILE} 2>&1
+
+echo "Sign libraries"
+
+find "${FULL_APP_NAME}" -iname '*.so' -or -iname '*.dylib' |
+    while read libfile; do
+        codesign --sign "${IDENTITY}" ${OPTIONS} "${libfile}" >> ${LOGFILE} 2>&1 ;
+    done;
+
+codesign --sign "${IDENTITY}" ${OPTIONS} "${FULL_APP_NAME}/Contents/MacOS/python"               >> ${LOGFILE} 2>&1
+codesign --sign "${IDENTITY}" ${OPTIONS} "${FULL_APP_NAME}/Contents/MacOS/${APPLICATION_NAME}"  >> ${LOGFILE} 2>&1
+
